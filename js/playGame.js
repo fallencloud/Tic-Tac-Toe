7944f53c90bd042be554bd72a5e7526d8005f44b
fallencloud <fallencloud@users.noreply.github.com>
2018-03-18 23:34:33 -0400

    Still not quite there...
    Such frustrate, so annoy...

--- js/playGame.js
+++ js/playGame.js
@@ -4,6 +4,31 @@ const $player1 = $('#player1');
 const $player2 = $('#player2');
 let isWon = false;
 let turnCounter = 0;
+let count = 1;
+
+for (let i = 0; i < $boxes.length; i++){
+    $boxes[i].classList.add(count);
+    count++;
+}
+
+function checkDiag() {
+    let diag1 = [1, 5, 9];
+    let diag2 = [3, 5, 7];
+}
+
+function checkCols() {
+    let col1 = [1, 4, 7];
+    let col2 = [2, 5, 8];
+    let col3 = [3, 6, 9];
+}
+
+function checkRows() {
+    let row1 = [1, 2, 3];
+    let row2 = [4, 5, 6];
+    let row3 = [7, 8, 9];
+}
+
+
 
 
 function showStart() {
@@ -20,20 +45,25 @@ function setColors() {
         $player1.addClass('active');
         $player2.removeClass('active');
 
-        $boxes.on('mouseenter mouseleave', function (event) {
-            $(this).toggleClass('box-filled-1');
+        $boxes.on('mouseenter', event => {
+            $(this).css("background-image", "url(../img/o.svg)");
         });
+
+        $boxes.on('mouseleave', )
+
+        // $boxes.on('mouseenter mouseleave', function (event) {
+        //     $(this).toggle(
+        //         $(this).css("background-image": );
+        //     );
+        // });
 
     } else {//ready player 2
         $player2.addClass('active');
         $player1.removeClass('active');
-        $boxes.on('mouseenter mouseleave', function (event) {
-            $(this).toggleClass('box-filled-2');
-        });
+        // $boxes.on('mouseenter mouseleave', function (event) {
+        //     $(this).toggleClass('box-filled-2');
+        // });
     }
-}
-function takeTurn() {
-    
 }
 
 function startGame() {
@@ -43,10 +73,15 @@ function startGame() {
 
    $boxes.on('click', function (event) {
         if (turnCounter % 2 === 0 ) {
-            $(this).addClass('box-filled-1');
+                if (!$(this).hasClass('box-filled-2')) {
+                    $(this).addClass('box-filled-1');
+            }
         } else {
-            $(this).addClass('box-filled-2');
+            if (!$(this).hasClass('box-filled-1')){
+                $(this).addClass('box-filled-2');
+            }
         }
+        
         turnCounter++;
         setColors();
     });   
